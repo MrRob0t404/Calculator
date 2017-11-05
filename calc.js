bb28ee7c9dee75f675f39b177859e225a933d950
simongaviria1 <simongaviria@ac.c4q.nyc>
2017-11-05 18:14:03 -0500

    Worked on JS file. Fully functional calculator

--- calc.js
+++ calc.js
@@ -1,31 +1,55 @@
 var outputDiv;
-
-var numbers = ['0','1','2','3','4','5','6','7','8','9'];
-var operators = ['+','-','*','/'];
-var operations = ['add','sub','mul','div']
+var equation = '';
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var operators = ['+', '-', '*', '/'];
+var operations = ['add', 'sub', 'mul', 'div']
 
 /**
  * @function calculate
  * @param  {string} mathExp {the expression to calculate}
  * @return {number} {the result of the calculation or NaN if it couldn't be done}
  */
-function calculate(mathExp){
-    try {
-       return eval(mathExp)
-    } catch (e) {
-       return NaN
-    }
+function calculate(mathExp) {
+  try {
+    return eval(mathExp)
+  } catch (error) {
+    return NaN
+  }
 }
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   outputDiv = document.getElementById('output');
 
-  document.addEventListener('click', function(event) {
+  document.addEventListener('click', function (event) {
     var elementId = event.target.id;
-    outputDiv.innerText += elementId;
+
+    if (numbers.includes(elementId)) {
+      outputDiv.innerText += elementId;
+    }
+
+    if (elementId === 'eq') {
+      outputDiv.innerText = calculate(equation);
+    } else if (numbers.indexOf(elementId) !== -1) {
+      equation += numbers[numbers.indexOf(elementId)];
+    } else {
+      equation += operators[operations.indexOf(elementId)]; 
+      outputDiv.innerText += operators[operations.indexOf(elementId)];
+    }
+
+    if(elementId === 'clr'){
+      equation = '';
+      outputDiv.innerText = ''
+    }
+
+    // if (elementId === 'eq') {
+    //   calculate(equation)
+    // }
+
     // logging the element id for debug purposes
     console.log(elementId)
-       
+    // console.log(outputDiv)
+    console.log(equation)
+    // console.log(calculate(equation))
   })
 })
